2767b9a76acbc54d183f0960a9f1aaaeeaf96889
widemar <widemarinternship@gmail.com>
2022-06-08 23:58:41 -0300

    Exercícios Java

---
+++ EstruturaCondicional/src/MenorDeTres.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MenorDeTres {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Primeiro valor: ");
+        int valor1 = scanner.nextInt();
+
+        System.out.print("Segundo valor: ");
+        int valor2 = scanner.nextInt();
+
+        System.out.print("Terceiro valor: ");
+        int valor3 = scanner.nextInt();
+
+        int menor;
+        if (valor1 < valor2 && valor1 < valor3) {
+            menor = valor1;
+        } else {
+            menor = Math.min(valor2, valor3);
+        }
+
+        System.out.printf("Menor = %d", menor);
+
+        scanner.close();
+    }
+}
