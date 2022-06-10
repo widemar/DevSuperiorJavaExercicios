7156706d3deef82af38dbb366ac4e10ae8100f38
widemar <widemarinternship@gmail.com>
2022-06-09 23:06:43 -0300

    Exercícios Java

---
+++ EstruturasRepetitivas/src/Tabuada.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Deseja a tabuada para qual valor: ");
+        int valor = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d\n", valor, i, valor * i);
+        }
+    }
+}
