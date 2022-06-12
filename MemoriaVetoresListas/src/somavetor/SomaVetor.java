5f3f5fec279c077a320f98a436b43d4842e8d1b8
widemar <widemarinternship@gmail.com>
2022-06-11 22:54:42 -0300

    Exercícios Java

---
+++ MemoriaVetoresListas/src/somavetor/SomaVetor.java
@@ -0,0 +1,34 @@
+package somavetor;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class SomaVetor {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Quantos números você vai digitar? ");
+        int qtdDeNumeros = scanner.nextInt();
+
+        double[] vetor = new double[qtdDeNumeros];
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print("Digite um número: ");
+            double num = scanner.nextDouble();
+            vetor[i] = num;
+        }
+
+        double soma = 0;
+        System.out.print("Valores: = ");
+        for (double v : vetor) {
+            soma += v;
+            System.out.printf("%.1f ", v);
+        }
+        double media = soma / vetor.length;
+        System.out.printf("\nSoma = %.2f\n", soma);
+        System.out.printf("Média = %.2f\n", media);
+        
+        scanner.close();
+    }
+}
