fc1c37f19e160a13608161250be365e6f5f5011b
widemar <widemarinternship@gmail.com>
2022-06-12 08:30:50 -0300

    Exercícios Java

---
+++ MemoriaVetoresListas/src/numerospares/NumerosPares.java
@@ -0,0 +1,34 @@
+package numerospares;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class NumerosPares {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Quantos números você vai digitar? ");
+        int qtdDeNumeros = scanner.nextInt();
+
+        int[] vetor = new int[qtdDeNumeros];
+
+        for (int i = 0; i < qtdDeNumeros; i++) {
+            System.out.print("Digite um número: ");
+            vetor[i] = scanner.nextInt();
+        }
+
+        int contaPar = 0;
+        System.out.println("Números pares:");
+        for (int i : vetor) {
+            if (i % 2 == 0) {
+                System.out.printf("%d ", i);
+                contaPar++;
+            }
+        }
+
+        System.out.printf("\nQuantidade de pares = %d", contaPar);
+
+        scanner.close();
+    }
+}
