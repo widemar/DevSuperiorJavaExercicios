e689df5c4192bc2207606f8b4713fe9a8f863266
widemar <widemarinternship@gmail.com>
2022-06-14 09:58:47 -0300

    Exercícios Java

---
+++ Matrizes/src/DiagonalNegativos.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class DiagonalNegativos {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Qual a ordem da matriz? ");
+        int tamanho = scanner.nextInt();
+
+        int[][] matriz = new int[tamanho][tamanho];
+        List<Integer> diagonaisPrincipais = new ArrayList<>();
+
+        int contaNegativos = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.printf("Elemento [%d,%d]: ", i, j);
+                int num = scanner.nextInt();
+                if (i == j) {
+                    diagonaisPrincipais.add(num);
+                }
+                if (num < 0) {
+                    contaNegativos++;
+                }
+            }
+        }
+
+        System.out.println("Diagonal principal:");
+        for (Integer diagonalPrincipal : diagonaisPrincipais) {
+            System.out.printf("%d ", diagonalPrincipal);
+        }
+        System.out.printf("Quantidade de negativos: %d", contaNegativos);
+
+        scanner.close();
+    }
+}
